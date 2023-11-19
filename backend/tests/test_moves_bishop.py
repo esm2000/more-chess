57962dbc63f65b78b38477585523aa573e29b995
Emeka Mbazor <embazor@gmail.com>
2023-11-19 01:36:13 -0800

    bishop blocked unit test

--- backend/tests/test_moves_bishop.py
+++ backend/tests/test_moves_bishop.py
@@ -103,7 +103,7 @@ def test_bishop_capture():
             [[6, 0], [6, 0]],
             [[7, 7], [7, 7]]
         ]) == sorted(possible_moves_and_captures["possible_captures"])
-        
+
     # black square
     for i in range(2):
         curr_game_state = copy.deepcopy(empty_game)
@@ -132,7 +132,76 @@ def test_bishop_capture():
 
 
 def test_bishop_blocked():
-    pass
+    # white square                      # black square
+    ##    0  1  2  3  4  5  6  7        ##    0  1  2  3  4  5  6  7
+    ## 0 |__|##|__|##|__|##|__|##|      ## 0 |__|##|__|##|__|##|__|##|
+    ## 1 |##|bp|##|__|##|__|##|__|      ## 1 |##|__|##|__|##|__|bp|__| 
+    ## 2 |__|##|wp|##|__|##|__|##|      ## 2 |__|##|__|##|__|wp|__|##|
+    ## 3 |##|__|##|wb|##|__|##|__|      ## 3 |##|__|##|__|wb|__|##|__|
+    ## 4 |__|##|__|##|__|##|__|##|      ## 4 |__|##|__|##|__|##|__|##|
+    ## 5 |##|wp|##|__|##|__|##|__|      ## 5 |##|__|##|__|##|__|wp|__|
+    ## 6 |bp|##|__|##|__|##|bp|##|      ## 6 |__|bp|__|##|__|##|__|bp|
+    ## 7 |##|__|##|__|##|__|##|bp|      ## 7 |bp|__|##|__|##|__|##|__|
+
+    ##    0  1  2  3  4  5  6  7        ##    0  1  2  3  4  5  6  7
+    ## 0 |__|##|__|##|__|##|__|##|      ## 0 |__|##|__|##|__|##|__|##|
+    ## 1 |##|wp|##|__|##|__|##|__|      ## 1 |##|__|##|__|##|__|wp|__| 
+    ## 2 |__|##|bp|##|__|##|__|##|      ## 2 |__|##|__|##|__|bp|__|##|
+    ## 3 |##|__|##|bb|##|__|##|__|      ## 3 |##|__|##|__|bb|__|##|__|
+    ## 4 |__|##|__|##|__|##|__|##|      ## 4 |__|##|__|##|__|##|__|##|
+    ## 5 |##|bp|##|__|##|__|##|__|      ## 5 |##|__|##|__|##|__|bp|__|
+    ## 6 |wp|##|__|##|__|##|wp|##|      ## 6 |__|wp|__|##|__|##|__|wp|
+    ## 7 |##|__|##|__|##|__|##|wp|      ## 7 |wp|__|##|__|##|__|##|__|
+
+    # white square
+    for i in range(2):
+        curr_game_state = copy.deepcopy(empty_game)
+        curr_game_state["board_state"][3][3] = [{"type": f"{'white' if not i else 'black'}_bishop"}]
+        curr_game_state["board_state"][1][1] = [{"type": f"{'black' if not i else 'white'}_pawn"}]
+        curr_game_state["board_state"][6][0] = [{"type": f"{'black' if not i else 'white'}_pawn"}]
+        curr_game_state["board_state"][7][7] = [{"type": f"{'black' if not i else 'white'}_pawn"}]
+
+        curr_game_state["board_state"][2][2] = [{"type": f"{'white' if not i else 'black'}_pawn"}]
+        curr_game_state["board_state"][5][1] = [{"type": f"{'white' if not i else 'black'}_pawn"}]
+        curr_game_state["board_state"][6][6] = [{"type": f"{'black' if not i else 'white'}_pawn"}]
+
+
+        prev_game_state = copy.deepcopy(curr_game_state)
+
+        curr_position = [3, 3]
+
+        possible_moves_and_captures = moves.get_moves_for_bishop(curr_game_state, prev_game_state, curr_position)
+        assert sorted([
+            [2, 4], [1, 5], [0, 6], [4, 2], [4, 4], [5, 5], [6, 6]
+        ]) == sorted(possible_moves_and_captures["possible_moves"])
+
+        assert [[7, 7], [7, 7]] not in possible_moves_and_captures["possible_captures"]
+        assert sorted([[[6, 6], [6, 6]]]) == sorted(possible_moves_and_captures["possible_captures"])
+    
+    # black square
+    for i in range(2):
+        curr_game_state = copy.deepcopy(empty_game)
+        curr_game_state["board_state"][3][4] = [{"type": f"{'white' if not i else 'black'}_bishop"}]
+        curr_game_state["board_state"][1][6] = [{"type": f"{'black' if not i else 'white'}_pawn"}]
+        curr_game_state["board_state"][6][7] = [{"type": f"{'black' if not i else 'white'}_pawn"}]
+        curr_game_state["board_state"][7][0] = [{"type": f"{'black' if not i else 'white'}_pawn"}]
+
+        curr_game_state["board_state"][2][5] = [{"type": f"{'white' if not i else 'black'}_pawn"}]
+        curr_game_state["board_state"][5][6] = [{"type": f"{'white' if not i else 'black'}_pawn"}]
+        curr_game_state["board_state"][6][1] = [{"type": f"{'black' if not i else 'white'}_pawn"}]
+
+
+        prev_game_state = copy.deepcopy(curr_game_state)
+
+        curr_position = [3, 4]
+
+        possible_moves_and_captures = moves.get_moves_for_bishop(curr_game_state, prev_game_state, curr_position)
+        assert sorted([
+            [2, 3], [1, 2], [0, 1], [4, 5], [4, 3], [5, 2], [6, 1]
+        ]) == sorted(possible_moves_and_captures["possible_moves"])
+
+        assert [[7, 0], [7, 0]] not in possible_moves_and_captures["possible_captures"]
+        assert sorted([[[6, 1], [6, 1]]]) == sorted(possible_moves_and_captures["possible_captures"])
 
 
 def test_bishop_cant_capture_king():

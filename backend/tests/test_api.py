01ae967826f56d23486fd544de209552cbf0f279
Emeka Mbazor <embazor@gmail.com>
2025-03-25 03:53:45 -0700

    add delay to game object creation for API integration tests

--- backend/tests/test_api.py
+++ backend/tests/test_api.py
@@ -1,6 +1,7 @@
 import copy
 from fastapi import HTTPException, Response
 import pytest
+import time
 
 from mocks.empty_game import empty_game
 import src.api as api
@@ -12,6 +13,7 @@ def game():
     game = api.create_game()
     yield game
     result = api.delete_game(game["id"])
+    time.sleep(5)
     assert result.get("message") == "Success"
     assert "not found" in api.retrieve_game_state(game["id"], Response()).get("message")
 

37a1d36134f67f6f1b4af1f3515a2203875d6205
esm2000 <47117694+esm2000@users.noreply.github.com>
2026-03-08 19:18:12 -0700

    Rename game to League of Chess (#23)

--- frontend/src/components/Title.js
+++ frontend/src/components/Title.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import { useIsMobile } from '../utility';
 
 
 const Title = () => {
-    const isMobile = useIsMobile()
-
     return(
         <div>
-            <div className="title" style={{ textAlign: isMobile ? 'center' : 'left' }}>
-                <h1 style={{display: "inline"}}>Chess</h1>
-                <h6 style={{display: "inline"}}>patch 1.1</h6>
-                <hr style={{clear:"both"}}/>
+            <div className="title">
+                <h1>League of Chess</h1>
+                <h6>patch 1.1</h6>
+                <hr/>
             </div>
         </div>
     );

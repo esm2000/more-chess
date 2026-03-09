f5bf5fb2b420a8fa0b23b6df391af3442223a086
Emeka Mbazor <embazor@gmail.com>
2026-03-08 19:16:35 -0700

    Address PR feedback: shorten manifest short_name and extract Title inline styles to CSS

--- frontend/src/components/Title.js
+++ frontend/src/components/Title.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import { useIsMobile } from '../utility';
 
 
 const Title = () => {
-    const isMobile = useIsMobile()
-
     return(
         <div>
-            <div className="title" style={{ textAlign: 'left' }}>
-                <h1 style={{display: "inline", wordSpacing: "-0.5em"}}>League of Chess</h1>
-                <h6 style={{display: "inline", marginLeft: "1em"}}>patch 1.1</h6>
-                <hr style={{clear:"both"}}/>
+            <div className="title">
+                <h1>League of Chess</h1>
+                <h6>patch 1.1</h6>
+                <hr/>
             </div>
         </div>
     );

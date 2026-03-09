eec41460e9d13e57dd4b66f35bbc203d9a4c6cb5
Emeka Mbazor <embazor@gmail.com>
2026-03-08 19:00:28 -0700

    Rename game to League of Chess and update favicon to dragon

--- frontend/src/components/Title.js
+++ frontend/src/components/Title.js
@@ -8,7 +8,7 @@ const Title = () => {
     return(
         <div>
             <div className="title" style={{ textAlign: isMobile ? 'center' : 'left' }}>
-                <h1 style={{display: "inline"}}>Chess</h1>
+                <h1 style={{display: "inline"}}>League of Chess</h1>
                 <h6 style={{display: "inline"}}>patch 1.1</h6>
                 <hr style={{clear:"both"}}/>
             </div>

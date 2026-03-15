eb5d1dd3ba4f6b8657aaf37291b1c3049ba518b7
Emeka Mbazor <embazor@gmail.com>
2026-03-15 10:55:32 -0700

    Add threading lock to lazy MongoDB client initialization

--- backend/src/database.py
+++ backend/src/database.py
@@ -2,31 +2,35 @@
 
 from dotenv import load_dotenv
 import os
+import threading
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
 load_dotenv()
 
 _mongo_client = None
+_lock = threading.Lock()
 
 
 def get_mongo_client():
     global _mongo_client
     if _mongo_client is None:
-        uri = os.environ.get("MONGODB_URI")
-        if uri:
-            if uri.startswith("mongodb+srv://"):
-                _mongo_client = MongoClient(uri, server_api=ServerApi("1"))
-            else:
-                _mongo_client = MongoClient(uri)
-        else:
-            host = os.environ["DB_HOST"]
-            username = os.environ["DB_USERNAME"]
-            password = os.environ["DB_PASSWORD"]
-            _mongo_client = MongoClient(
-                f"mongodb+srv://{username}:{password}@{host}/?retryWrites=true&w=majority",
-                server_api=ServerApi("1"),
-            )
+        with _lock:
+            if _mongo_client is None:
+                uri = os.environ.get("MONGODB_URI")
+                if uri:
+                    if uri.startswith("mongodb+srv://"):
+                        _mongo_client = MongoClient(uri, server_api=ServerApi("1"))
+                    else:
+                        _mongo_client = MongoClient(uri)
+                else:
+                    host = os.environ["DB_HOST"]
+                    username = os.environ["DB_USERNAME"]
+                    password = os.environ["DB_PASSWORD"]
+                    _mongo_client = MongoClient(
+                        f"mongodb+srv://{username}:{password}@{host}/?retryWrites=true&w=majority",
+                        server_api=ServerApi("1"),
+                    )
     return _mongo_client
 
 
